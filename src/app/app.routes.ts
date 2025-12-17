c3ac4d98b2221bd202b851d87af78f521ea10f34
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-17 09:07:16 +0100

    feat(router): Add auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './auth/auth-layout/auth-layout';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "auth",
+        component: AuthLayout,
+        children: [
+            {
+                path: "login",
+                component: Login
+            },
+            {
+                path: "register",
+                component: Register
+            }
+        ]
+    }
+];
