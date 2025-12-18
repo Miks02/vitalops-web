72e7c86e2d8b96a06398e000e5a9121988903fc5
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-18 15:30:19 +0100

    feat(component): Add more links to the sidebar

--- src/app/layout/utilities/sidebar/sidebar.ts
+++ src/app/layout/utilities/sidebar/sidebar.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { faSolidUser, faSolidBook, faSolidChartLine, faSolidGear, faSolidDumbbell, faSolidFireFlameCurved} from '@ng-icons/font-awesome/solid';
+import { faSolidUser, faSolidBookOpen, faSolidChartLine, faSolidGear, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon} from '@ng-icons/font-awesome/solid';
 
 @Component({
     selector: 'app-sidebar',
     imports: [NgIcon],
     templateUrl: './sidebar.html',
     styleUrl: './sidebar.css',
-    providers: [provideIcons({faSolidUser, faSolidChartLine, faSolidGear, faSolidBook, faSolidDumbbell, faSolidFireFlameCurved})]
+    providers: [provideIcons({faSolidUser, faSolidChartLine, faSolidGear, faSolidBookOpen, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon})]
 })
 export class Sidebar {
 
