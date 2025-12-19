47b09251640838bb71a598359e1f06c692560186
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-19 14:13:34 +0100

    wip(component): Add a mobile responsive sidebar (needs toogle functionality)

--- src/app/layout/utilities/sidebar/sidebar.ts
+++ src/app/layout/utilities/sidebar/sidebar.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { faSolidUser, faSolidBookOpen, faSolidChartLine, faSolidGear, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon} from '@ng-icons/font-awesome/solid';
+import { faSolidUser, faSolidBookOpen, faSolidChartLine, faSolidGear, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon, faSolidCircleChevronLeft, faSolidRightToBracket} from '@ng-icons/font-awesome/solid';
 
 @Component({
     selector: 'app-sidebar',
     imports: [NgIcon],
     templateUrl: './sidebar.html',
     styleUrl: './sidebar.css',
-    providers: [provideIcons({faSolidUser, faSolidChartLine, faSolidGear, faSolidBookOpen, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon})]
+    providers: [provideIcons({faSolidUser, faSolidChartLine, faSolidGear, faSolidBookOpen, faSolidDumbbell, faSolidBowlRice, faSolidArrowUpRightDots, faSolidMoon, faSolidCircleChevronLeft, faSolidRightToBracket})]
 })
 export class Sidebar {
 
