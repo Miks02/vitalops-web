715634b16e7ac7165e11ce895ba0d77e593caf5d
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-19 02:10:38 +0100

    styles(component): Adjust the styles of app-layout component

--- src/app/layout/app-layout/app-layout.ts
+++ src/app/layout/app-layout/app-layout.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Sidebar } from '../utilities/sidebar/sidebar';
 import { Header } from '../utilities/header/header';
 import { RouterOutlet } from "@angular/router";
+import { BottomNav } from "../utilities/bottom-nav/bottom-nav";
 
 @Component({
-  selector: 'app-app-layout',
-  imports: [Sidebar, Header, RouterOutlet],
-  templateUrl: './app-layout.html',
-  styleUrl: './app-layout.css',
+    selector: 'app-app-layout',
+    imports: [Sidebar, Header, RouterOutlet, BottomNav],
+    templateUrl: './app-layout.html',
+    styleUrl: './app-layout.css',
 
 
 })
