bd4aaa545d62193dc366591a3c063bd5c944c893
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-15 12:36:47 +0100

    feat(service): Update getProfilePage() method so that it fills userDetails in UserService after being subscribed on it

--- src/app/pages/profile/services/profile-service.ts
+++ src/app/pages/profile/services/profile-service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, map, tap } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 import { ProfilePageDto } from '../models/ProfilePageDto';
 import { ApiResponse } from '../../../core/models/ApiResponse';
+import { UserService } from '../../../core/services/user-service';
 
 @Injectable({
     providedIn: 'root',
@@ -12,6 +13,7 @@ export class ProfileService {
     private api = environment.apiUrl;
 
     private http = inject(HttpClient);
+    private userService = inject(UserService);
 
     private profilePageSubject = new BehaviorSubject<ProfilePageDto | undefined>(undefined);
     public profilePage$ = this.profilePageSubject.asObservable();
@@ -19,9 +21,10 @@ export class ProfileService {
     getProfilePage() {
         return this.http.get<ProfilePageDto>(`${this.api}/profile`).pipe(
             tap(res => {
-                this.profilePageSubject.next(res)
+                this.userService.userDetails = res.userDetails
+                this.profilePageSubject.next(res);
             }),
-            
+
         );
     }
 }
