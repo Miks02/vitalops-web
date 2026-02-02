a0762d41a4c40145e26af0b6a3f2b3e281d7fb96
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-02-02 09:13:56 +0100

    styles(component): Add minor ui changes to the password-form

--- src/app/pages/profile/password-form/password-form.ts
+++ src/app/pages/profile/password-form/password-form.ts
@@ -1,24 +1,22 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { faSolidKey, faSolidLock, faSolidXmark } from "@ng-icons/font-awesome/solid";
+import { faSolidCircleInfo, faSolidInfo, faSolidKey, faSolidLock, faSolidXmark } from "@ng-icons/font-awesome/solid";
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { createChangePasswordForm } from '../../../core/helpers/Factories';
 import { isControlValid } from '../../../core/helpers/FormHelpers';
-import { UserService } from '../../../core/services/user-service';
 import { NotificationService } from '../../../core/services/notification-service';
-import { take } from 'rxjs';
 
 @Component({
     selector: 'app-password-form',
     imports: [NgIcon, FormsModule, ReactiveFormsModule],
     templateUrl: './password-form.html',
     styleUrl: './password-form.css',
-    providers: [provideIcons({faSolidKey, faSolidLock, faSolidXmark})]
+    providers: [provideIcons({faSolidKey, faSolidLock, faSolidXmark, faSolidCircleInfo})]
 })
 export class PasswordForm {
     @Output()
     close = new EventEmitter<void>();
-    
+
     private fb = inject(FormBuilder);
     private notificationService = inject(NotificationService);
 
